25a26d1120c7e068ca1679a5b297750e64ff8cee
benhuynh003 <155077782+benhuynh003@users.noreply.github.com>
2024-08-17 16:04:49 -0700

    setting up cleark / nav bar

--- app/page.js
+++ app/page.js
@@ -1,3 +1,27 @@
+"use client";
+
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { AppBar, Button, Typography, Toolbar } from "@mui/material";
+
 export default function Home() {
-  return <></>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          Woken AI 🏯
+        </Typography>
+        <SignedOut>
+          <Button color="inherit" href="/sign-in">
+            Login
+          </Button>
+          <Button color="inherit" href="/sign-up">
+            Sign Up
+          </Button>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Toolbar>
+    </AppBar>
+  );
 }
