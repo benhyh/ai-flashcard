d81bc057ac1bcc89985e49692dfeff08525ea687
benhuynh003 <155077782+benhuynh003@users.noreply.github.com>
2024-08-17 18:03:14 -0700

    Added features section & headers for demo video and pricing (TBD)

---
+++ app/components/Demo.js
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+export const Demo = () => {
+  return (
+    <Box>
+      <Typography variant="h4" component="h2" gutterBottom align="center">
+        Demo Video
+      </Typography>
+    </Box>
+  );
+};
+
+export default Demo;
--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
 "use client";
 
+import { Demo } from "./components/Demo";
 import { Features } from "./components/Features";
 import { Navigation } from "./components/navigation";
+import { Pricing } from "./components/Pricing";
 
 export default function Home() {
   return (
     <>
       <Navigation />
       <Features />
+      <Demo />
+      <Pricing />
     </>
   );
 }
